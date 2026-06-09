a806b398cc42e189d6137e027e9bcf7418758b44
codermegasuper <tomaermolaeva2106@gmail.com>
2026-06-09 20:56:58 +0500

    update matrix and UI

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ console.log("TermuxHub Loaded");
 const canvas = document.getElementById("matrix");
 const ctx = canvas.getContext("2d");
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+function resize(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resize();
+window.addEventListener("resize", resize);
 
 let letters = "01TERMUXHUB01";
 letters = letters.split("");
@@ -11,24 +16,23 @@ letters = letters.split("");
 let fontSize = 14;
 let columns = canvas.width / fontSize;
 
-let drops = [];
-
-for(let x = 0; x < columns; x++)
-    drops[x] = 1;
+let drops = Array(Math.floor(columns)).fill(1);
 
 function draw(){
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0,0,0,0.08)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
 
     ctx.fillStyle = "#00ff66";
     ctx.font = fontSize + "px monospace";
 
-    for(let i = 0; i < drops.length; i++){
+    for(let i=0;i<drops.length;i++){
         let text = letters[Math.floor(Math.random()*letters.length)];
+
         ctx.fillText(text, i*fontSize, drops[i]*fontSize);
 
-        if(drops[i]*fontSize > canvas.height && Math.random() > 0.975)
-            drops[i] = 0;
+        if(drops[i]*fontSize > canvas.height && Math.random()>0.975){
+            drops[i]=0;
+        }
 
         drops[i]++;
     }
